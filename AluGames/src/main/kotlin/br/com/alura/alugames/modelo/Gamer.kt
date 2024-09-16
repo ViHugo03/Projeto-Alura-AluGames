4c39f26197ab6fc3955f639d910c16a8947b8a94
ViHugo03 <99813230+ViHugo03@users.noreply.github.com>
2024-09-15 23:36:05 -0300

    Criando Classe Gamer e implementando o let com also

--- AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,16 +1,49 @@
 package br.com.alura.alugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome: String, var email: String) {
     var dataNascimento: String? = null
     var usuario: String? = null
-    var idInternio: String? = null
+        set(value) {
+            field = value
+            if (idInterno.isNullOrBlank()){
+                criarIdInterno()
+            }
+        }
+    var idInterno: String? = null
+        private set
+
+    init {
+        if (nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome não pode ser vazio")
+        }
+        this.email = validarEmail()
+    }
 
     constructor(nome: String, email: String, dataNascimento: String, usuario: String) : this(nome, email) {
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        criarIdInterno()
     }
 
+
+
     override fun toString(): String {
-        return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInternio=$idInternio)"
+        return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
+    }
+
+    fun criarIdInterno() {
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+        this.idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail(): String {
+        val regex = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\\.[a-zA-Z.]{2,18}")
+        if (regex.matches(email)) {
+            return email
+        }
+        throw IllegalArgumentException("Email inválido")
     }
 }
---
+++ AluGames/src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -0,0 +1,22 @@
+package br.com.alura.alugames.principal
+
+import br.com.alura.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer("Jacque","jacque@email.com")
+    println(gamer1)
+
+    val gamer2 = Gamer("Jeni","jeni@email.com","19/19/1992","jeniblo")
+
+    println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "19/19/1992"
+        it.usuario = "jacqueskywalker"
+    }.also { println(gamer1.idInterno) }
+
+    println(gamer1)
+
+    gamer1.usuario = "jeniblo"
+   println(gamer1.idInterno)
+}
